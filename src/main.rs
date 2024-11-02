b2ec9d976d88b486639490aa12dc91f55ff38ac1
diogogomesaraujo <118282799+diogogomesaraujo@users.noreply.github.com>
2024-11-02 18:32:15 +0000

    create message schedule and fixing the pre_processing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::io::{self, Write};
+use std::fs;
+use std::env;
 
 const PRIME_NUMBERS: &[i32] = &[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311];
 
@@ -26,44 +27,58 @@ fn start_round_constants() -> Vec<u32>{
 
 fn pre_processing(message: String) -> Vec<u8>{
     let mut bytes: Vec<u8> = message.into_bytes(); // convert to vetor of bytes
-    let mut message_size = bytes.len();
+    let message_size = bytes.len();
 
-    bytes.push(128 as u8);
+    bytes.push(0b10000000);
 
-    if message_size + 1 + 8 <= 64{ // isize plus the max space the one can occupy plus the max size of the string
-        for _i in 0..(64 - bytes.len() - 8){ // iterate through all the 0 space
-            bytes.push(0 as u8);
-        }
-
-        message_size *= 8;
-        bytes.extend_from_slice(&message_size.to_be_bytes());
-    } else{
-        let aux_num_block = (message_size + 1 + 8) / 64; // get the size in 512 base
-        for _i in 1..aux_num_block{
-            bytes.push(0 as u8);
-        }
-
-        message_size *= 8;
-        bytes.extend_from_slice(&message_size.to_be_bytes());
+    // padd untill 448
+    while (bytes.len() % 64) != 56{
+        bytes.push(0);
     }
+
+    let message_size = (message_size * 8) as u64;
+    bytes.extend_from_slice(&message_size.to_be_bytes());
 
     bytes
 }
 
-fn main() {
-    print!("Write a message for the kitten to hash -> ");
-    io::stdout().flush().unwrap();
+fn create_message_schedule(pre_processed_message: Vec<u8>) -> Vec<Vec<u32>>{
+    let mut schedules: Vec<Vec<u32>> = vec![];
+    for block in pre_processed_message.chunks(64){ // for each 512bit chunk
+        let mut w: Vec<u32> = block.chunks(4)
+            .map(|chunk| {
+                (chunk[0] as u32) << 24 |
+                (chunk[1] as u32) << 16 |
+                (chunk[2] as u32) << 8  |
+                (chunk[3] as u32)
+            }).collect();
 
-    let mut message = String::new();
-    io::stdin()
-        .read_line(&mut message)
-        .expect("Failed to read the message!");
+        w.resize(64, 0); // fill with 0 what is missing
+        
+        for i in 16..64{
+            let s0: u32 = (w[i - 15].rotate_right(7)) ^ (w[i - 15].rotate_right(18)) ^ (w[i-15] >> 3);
+            let s1: u32 = (w[i - 2].rotate_right(17)) ^ (w[i - 2].rotate_right(19)) ^ (w[i - 2] >> 10);
+        
+            w[i] = (w[i - 16].wrapping_add(s0).wrapping_add(w[i - 7]).wrapping_add(s1));
+        }
 
-    let bytes: Vec<u8> = pre_processing(message.trim_end().to_string());
-
-    for byte in bytes{
-        print!("{:08b} ", byte);
-        io::stdout().flush().unwrap();
+        schedules.push(w);
     }
-    println!();
+
+    schedules
+}
+
+fn main() {
+    let args:Vec<String> = env::args().collect();
+
+    let file_path = &args[1];
+
+    let content = fs::read_to_string(file_path)
+        .expect("The kitten could not read the file!");
+
+    let content_bytes = pre_processing(content);
+    println!("{:?}\n", content_bytes);
+
+    let schedule = create_message_schedule(content_bytes);
+    println!("{:?}\n", schedule);
 }
